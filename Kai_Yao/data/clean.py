e20c380e36d6dc800645197618d535a9dcf7e281
kyao4 <kyao4@hawk.iit.edu>
2016-10-26 20:58:33 -0500

    add lengdary

--- Kai_Yao/data/clean.py
+++ Kai_Yao/data/clean.py
@@ -17,11 +17,12 @@ def main():
             Total = each[4]
             HP = each[5]
             Attack = each[6]
-            ATTSP = each[7]
-            DEFSP = each[8]
-            Speed = each[9]
-            Generation = each[10]
-            Lengendary = each[11]
+            Defense = each[7]
+            ATTSP = each[8]
+            DEFSP = each[9]
+            Speed = each[10]
+            Generation = each[11]
+            Lengendary = each[12]
             num = "'?'" if num == "" else num
             Name = "'?'" if Name == "" else Name
             Type1 = "'?'" if Type1 == "" else Type1
@@ -29,6 +30,7 @@ def main():
             Total = "'?'" if Total == "" else Total
             HP = "'?'" if HP == "" else HP
             Attack = "'?'" if Attack == "" else Attack
+            Defense = "'?'" if Defense == "" else Defense
             ATTSP = "'?'" if ATTSP == "" else ATTSP
             DEFSP = "'?'" if DEFSP == "" else DEFSP
             Speed = "'?'" if Speed == "" else Speed
@@ -36,7 +38,7 @@ def main():
             Lengendary = "'?'" if Lengendary == "" else Lengendary
 #             if num == "" or Name == "" or Type1 == "" or Type2 == "" or Total == "" or HP == "" or Attack == "" or ATTSP == "" or DEFSP == "" or Speed == "" or Generation == "" or Lengendary == "":
 #                     continue
-            pokemon = [num, Name, Type1, Type2 , Total, HP, Attack, ATTSP, DEFSP, Speed, Generation, Lengendary]
+            pokemon = [num, Name, Type1, Type2 , Total, HP, Attack, Defense, ATTSP, DEFSP, Speed, Generation, Lengendary]
             myList.append(pokemon) 
         with open('Pokemon_output.csv', 'w', newline='', encoding='utf-8') as csvfile:
             spamwriter = csv.writer(csvfile, delimiter=',')
